4c38c50bbc5354cfd13fb815cb38b00fee013d6b
AndreaDeStefano1 <enricoandrea.destefano@gmail.com>
2022-05-02 17:20:51 +0200

    Esercizio su introduzione vue.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        message : 'Hello world',
+        image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--VtIgiqJe--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://thepracticaldev.s3.amazonaws.com/i/d440mmj72v2vi7ad76ir.png',
+    }
+})
